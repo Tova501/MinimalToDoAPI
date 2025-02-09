afb79f70a195462f54ff91d2d2abf6e6f06ef3f4
Tova501 <ta0556736501@gmail.com>
2025-02-09 02:00:07 +0200

    fix client

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -1,37 +1,4 @@
-// import axios from 'axios';
 
-// export default {
-//   getTasks: async () => {
-//     try {
-//         const response = await axios.get('/items', {
-//         });
-//         return response.data;
-//     } catch (error) {
-//         throw error;
-//     }
-//   },
-//   addTask: async (name) => {
-//     axios.post('/items', {
-//        id: 0, name: name, isComplete: false
-//     }).then(result => {
-//       return getTasks();
-//     }).then(e =>{
-//       console.log(e.message);
-//     })
-//   },
-
-//   setCompleted: async (id, isComplete) => {
-//     console.log('setCompleted', { id, isComplete })
-//     //TODO
-//     return {};
-//   },
-
-//   deleteTask: async () => {
-//     console.log('deleteTask')
-//   }
-// };
-
-import axios from 'axios';
 import myAxios from './axiosConfiguration';
 
 export default {
