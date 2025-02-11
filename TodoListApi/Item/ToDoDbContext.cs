3e43f3c05d2c4f3e54357a8ba5215af7bd4997bc
Tova501 <ta0556736501@gmail.com>
2025-02-11 21:33:20 +0200

    change db context

--- TodoListApi/Item/ToDoDbContext.cs
+++ TodoListApi/Item/ToDoDbContext.cs
@@ -1,3 +1,56 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using Microsoft.EntityFrameworkCore;
+// using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+// namespace TodoApi.Item;
+
+// public partial class ToDoDbContext : DbContext
+// {
+//     public ToDoDbContext()
+//     {
+//     }
+
+//     public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+//         : base(options)
+//     {
+//     }
+
+//     public virtual DbSet<Item> Items { get; set; }
+
+// protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// {
+//     if (!optionsBuilder.IsConfigured)
+//     {
+//         optionsBuilder.UseMySql(
+//             "Server=bpsj0wg29cg5dtklnssy-mysql.services.clever-cloud.com;Port=3306;Database=bpsj0wg29cg5dtklnssy;User=uwkx4isqy2sspcyj;Password=swakR3M8hqvMSepkcMBi;",
+//             Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"));
+//     }
+// }
+
+//     protected override void OnModelCreating(ModelBuilder modelBuilder)
+//     {
+//         modelBuilder
+//             .UseCollation("utf8mb4_0900_ai_ci")
+//             .HasCharSet("utf8mb4");
+
+//         modelBuilder.Entity<Item>(entity =>
+//         {
+//             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+//             entity.ToTable("Items");
+
+//             entity.Property(e => e.Id);
+//             entity.Property(e => e.IsComplete);
+//             entity.Property(e => e.Name)
+//                 .HasMaxLength(100);
+//         });
+
+//         OnModelCreatingPartial(modelBuilder);
+//     }
+
+//     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+// }
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
@@ -18,16 +71,6 @@ public partial class ToDoDbContext : DbContext
 
     public virtual DbSet<Item> Items { get; set; }
 
-protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-{
-    if (!optionsBuilder.IsConfigured)
-    {
-        optionsBuilder.UseMySql(
-            "Server=bpsj0wg29cg5dtklnssy-mysql.services.clever-cloud.com;Port=3306;Database=bpsj0wg29cg5dtklnssy;User=uwkx4isqy2sspcyj;Password=swakR3M8hqvMSepkcMBi;",
-            Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"));
-    }
-}
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
@@ -38,12 +81,9 @@ protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
 
-            entity.ToTable("Items");
+            entity.ToTable("items");
 
-            entity.Property(e => e.Id);
-            entity.Property(e => e.IsComplete);
-            entity.Property(e => e.Name)
-                .HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(100);
         });
 
         OnModelCreatingPartial(modelBuilder);
